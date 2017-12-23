95fb270c365627c50b943f9a5e437842b03c4520
haiwei510 <1836155521@qq.com>
2017-12-23 14:44:59 +0800

    去掉没用的类

--- src/main/java/com/duanying/controller/TestController.java
+++ src/main/java/com/duanying/controller/TestController.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.duanying.model.User;
-import com.duanying.model.Users;
 import com.duanying.service.UserServiceI;
 
 @Controller
